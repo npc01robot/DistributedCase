8b283e7777b0f19867c6174842a39a98981df70c
zhouguoquan <zhouguoquan@starmerx.com>
2024-07-08 14:27:05 +0800

    init

--- user/src/main/java/com/robot/user/UserApplication.java
+++ user/src/main/java/com/robot/user/UserApplication.java
@@ -1,8 +1,10 @@
 package com.robot.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.robot.user.mapper")
 @SpringBootApplication
 public class UserApplication {
 
